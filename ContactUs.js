56f8182ecc5a86f093a4072af4f3a11c5537f1f1
Jeff Juell <87201199+jefejuell@users.noreply.github.com>
2021-10-27 23:00:02 -0500

    Project updates

--- ContactUs.js
+++ ContactUs.js
@@ -75,20 +75,22 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiamVmZWp1ZWxsIiwiYSI6ImNrdGZ6aTlpaTBkOWczMm5sM
         center:  [-90.370760, 38.636210],
         zoom: 10
         });
-    //Adds navigation controls
-map.addControl(new mapboxgl.NavigationControl());
+    
     //Adds location markers
 const marker = new mapboxgl.Marker()
     .setLngLat([-90.470760, 38.636210])
     .addTo(map);
     
-const marker2 = new mapboxgl.Marker()
+const marker2 = new mapboxgl.Marker({color:'green'})
     .setLngLat([-90.495769, 38.701884])
     .addTo(map);
 
-const marker3 = new mapboxgl.Marker()
+const marker3 = new mapboxgl.Marker({color:'green'})
     .setLngLat([-90.270020, 38.635078])
     .addTo(map);    
+
+    //Adds navigation controls
+    map.addControl(new mapboxgl.NavigationControl());
 
     //Add clickable interface that enables user to change map styles
 const layerList = document.getElementById('menu');
@@ -101,70 +103,7 @@ const inputs = layerList.getElementsByTagName('input');
         };
     }    
 
-
-// /* Given a query in the form "lng, lat" or "lat, lng"
-// * returns the matching geographic coordinate(s)
-// * as search results in carmen geojson format,
-// * https://github.com/mapbox/carmen/blob/master/carmen-geojson.md */
-// const coordinatesGeocoder = function (query) {
-//     // Match anything which looks like
-//     // decimal degrees coordinate pair.
-//     const matches = query.match(
-//     /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
-//     );
-//     if (!matches) {
-//     return null;
-//     }
-     
-//     function coordinateFeature(lng, lat) {
-//         return {
-//             center: [lng, lat],
-//             geometry: {
-//             type: 'Point',
-//             coordinates: [lng, lat]
-//             },
-//             place_name: 'Lat: ' + lat + ' Lng: ' + lng,
-//             place_type: ['coordinate'],
-//             properties: {},
-//             type: 'Feature'
-//         };
-//     }
-     
-//     const coord1 = Number(matches[1]);
-//     const coord2 = Number(matches[2]);
-//     const geocodes = [];
-     
-//     if (coord1 < -90 || coord1 > 90) {
-//     // must be lng, lat
-//         geocodes.push(coordinateFeature(coord1, coord2));
-//     }
-     
-//     if (coord2 < -90 || coord2 > 90) {
-//     // must be lat, lng
-//         geocodes.push(coordinateFeature(coord2, coord1));
-//     }
-     
-//     if (geocodes.length === 0) {
-//     // else could be either lng, lat or lat, lng
-//         geocodes.push(coordinateFeature(coord1, coord2));
-//         geocodes.push(coordinateFeature(coord2, coord1));
-//     }
-     
-//     return geocodes;
-//     };
-     
-//     // Add the control to the map.
-// map.addControl(
-//     new MapboxGeocoder({
-//     accessToken: mapboxgl.accessToken,
-//     localGeocoder: coordinatesGeocoder,
-//     zoom: 4,
-//     placeholder: 'Try: -40, 170',
-//     mapboxgl: mapboxgl,
-//     reverseGeocode: true
-//     })
-// );
-
+//OLD Code below
     /*if (serviceLevel.toLowerCase() === 'minor') {
         cost = yardCost[0][1];
       } else if (serviceLevel.toLowerCase() === 'normal') {
